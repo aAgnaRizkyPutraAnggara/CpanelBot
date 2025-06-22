6e35fbbddca2da4d417ef29cbd8380e311a144c5
Jhonaley Barbosa <132731456+aAgnaRizkyPutraAnggara@users.noreply.github.com>
2025-06-23 03:18:41 +0700

    Add files via upload

---
+++ src/message.js
@@ -0,0 +1,222 @@
+global.menu = `
+┱📜 MENU BOT
+┢─────
+┣ /ownermenu
+┣ /storemenu
+┣ /cpanelowner
+┣ /resellerpanel
+┣ /toolsmenu
+┣ /installermenu
+┣ /downloadermenu
+╿
+┹
+`
+
+global.ownermenu = `
+┱ 📃OwnerMenu
+┢────────
+┣ /connect
+┣ /addseller
+┣ /delseller
+┣ /listseller
+┣ /addreseller_prvt
+┣ /delreseller_prvt
+┣ /listreseller_prvt
+┣ /addpartner
+┣ /delpartner
+┣ /listpartner
+┣ /addpartner_prvt
+┣ /delpartner_prvt
+┣ /listpartner_prvt
+┣ /restart
+┣ /shutdown
+┣ /addlowner
+┣ /delowner
+┣ /listowner
+┣ /setapi
+┣ /setcapi
+┣ /setapi_prvt
+┣ /setcapi_prvt
+┣ /seturl
+┣ /seturl_prvt
+╿
+┹
+`
+
+// Mengganti nama CpanelOwnerMenu menjadi PartnerPanelPublik
+global.partnerpanel_public = `
+┱ 📃PartnerPanelPublik
+┢─────────────
+┣ /1gb
+┣ /2gb
+┣ /3gb
+┣ /4gb
+┣ /5gb
+┣ /6gb
+┣ /7gb
+┣ /8gb
+┣ /9gb
+┣ /10gb
+┣ /unli
+┣ /createadmin
+┣ /listpanel
+┣ /listusr
+┣ /listadmin
+┣ /Delsrv
+┣ /Deladmin
+┣ /Delusr
+╿
+┹
+`
+
+// Mengganti nama CpanelPrivateMenu menjadi PartnerPanelPrivate
+global.partnerpanel_private = `
+┱ 📃PartnerPanelPrivate
+┢─────────────
+┣ /delusr_prvt
+┣ /listusr_prvt
+┣ /createadmin_prvt
+┣ /delsrv_prvt
+┣ /listpanel_prvt
+┣ /1gb_prvt
+┣ /2gb_prvt
+┣ /3gb_prvt
+┣ /4gb_prvt
+┣ /5gb_prvt
+┣ /6gb_prvt
+┣ /7gb_prvt
+┣ /8gb_prvt
+┣ /9gb_prvt
+┣ /10gb_prvt
+┣ /unli_prvt
+╿
+┹
+`
+
+global.resellerpanel = `
+┱ 📃ResellerPanelPublik
+┢─────────────
+┣ /1gb
+┣ /2gb
+┣ /3gb
+┣ /4gb
+┣ /5gb
+┣ /6gb
+┣ /7gb
+┣ /8gb
+┣ /9gb
+┣ /10gb
+┣ /unli
+`
+
+// Tambahkan menu Reseller Panel Private
+global.resellerpanel_private = `
+┱ 📃ResellerPanelPrivate
+┢─────────────
+┣ /1gb_prvt
+┣ /2gb_prvt
+┣ /3gb_prvt
+┣ /4gb_prvt
+┣ /5gb_prvt
+┣ /6gb_prvt
+┣ /7gb_prvt
+┣ /8gb_prvt
+┣ /9gb_prvt
+┣ /10gb_prvt
+┣ /unli_prvt
+╿
+┹
+`
+
+global.toolsmenu = `
+┱ 📃ToolsMenu
+┢────────
+┣ /cekidtele
+┣ /tourl
+┣ /toqr
+┣ /sticker
+┣ /toimg
+┣ /tovideo
+┣ /remini
+┣ /brat
+┣ /qc
+╿
+┹
+`
+
+global.downloadermenu = `
+┱ 📃DownloadMenu
+┢───────────
+┣ /tiktok
+┣ /tiktokslide
+┣ /igdl
+┣ /ytdl
+┣ /spotifydl
+┣ /ssweb
+┣ /pinterest
+╿
+┹
+`
+
+global.installermenu = `
+┱ 📃InstallerMenu
+┢─────────
+┣ /installpanel
+┣ /startwings
+┣ /uninstallpanel
+┣ /installtemastellar
+┣ /installtemaenigma
+┣ /installtemadarknate
+┣ /installtemabilling
+┣ /installtemaiceminecraft
+┣ /installtemanook
+┣ /installtemanightcore
+┣ /uninstalltema
+╿
+┹
+`
+
+global.storemenu = `
+┱ 📃StoreMenu
+┢───────
+┣ /qris
+┣ /changeqris
+┣ /listproduk
+┣ /addlist
+┣ /dellist
+┣ /updatelist
+┣ /dellistall
+┣ /
+┣ /
+┣ /
+╿
+┹
+`
+
+global.groupmenu = `
+┱ 📃Groupmenu
+┢───────
+┣ /add
+┣ /kick
+┣ /welcome
+┣ /leave
+┣ /promote
+┣ /demote
+┣ /open
+┣ /close
+┣ /antilink
+┣ /changetitle
+┣ /changedesk
+┣ /changeppgc
+┣ /delete
+┣ /pin
+┣ /unpin
+┣ /createpolling
+┣ /groupstats
+┣ /linkgroup
+┣ /warn
+┣ /warns
+┣ /resetwarn
+╿
+┹
+`
